52095a2b5ed922f55a6d0c3e5ca4d95009899e7e
JinTaeYeong <jjhtkfkd12@naver.com>
2021-09-29 17:01:31 +0900

    [2021-9-29] 프로젝트 업로드

---
+++ Assets/odin.cs
@@ -0,0 +1,31 @@
+﻿// SceneAndAssetsOnlyExamplesComponent.cs
+using Sirenix.OdinInspector;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class odin : MonoBehaviour
+{
+
+    [Title("Assets only")]
+    [AssetsOnly]
+    public List<GameObject> OnlyPrefabs;
+
+    [AssetsOnly]
+    public GameObject SomePrefab;
+
+    [AssetsOnly]
+    public Material MaterialAsset;
+
+    [AssetsOnly]
+    public MeshRenderer SomeMeshRendererOnPrefab;
+
+    [Title("Scene Objects only")]
+    [SceneObjectsOnly]
+    public List<GameObject> OnlySceneObjects;
+
+    [SceneObjectsOnly]
+    public GameObject SomeSceneObject;
+
+    [SceneObjectsOnly]
+    public MeshRenderer SomeMeshRenderer;
+}
